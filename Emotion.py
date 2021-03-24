8345754ad39fa773c8a06d90812f4dc06b602761
fadybassel1 <56454311+fadybassel1@users.noreply.github.com>
2021-03-24 05:58:16 +0200

    improvements

--- Emotion.py
+++ Emotion.py
@@ -1,54 +1,58 @@
 import cv2
+
+
 class DetectEmotionController:
-    smallVideos=[]
     def __init__(self,smallVideos):
         self.smallVideos = smallVideos
+        self.detectFaces = DetectFaces()
+        self.emotionModel = EmotionModel()
 
     #### main function #####
     def extract_emotions(self,secondJump = 1):
         results = []
         for video in self.smallVideos:
-           frames = self.extract_frames(video,secondJump)
-           faceDetection=DetectFaces(frames)
-           faceImages = faceDetection.detect()
-           model = EmotionModel(faceImages)
-           results.append(model.predict())
+           frames = ExtractFrames.extract_frames(video,secondJump)
+           faceImages = self.detectFaces.detect(frames)
+           results.append(self.emotionModel.predict(faceImages))
         return results
     
-    def extract_frames(self,video,secondJump):
-        framesList = []
-        cap=cv2.VideoCapture(video)
-        if not cap.isOpened():
-            print('cap was not opened')
-            cap.open(video)
-        # count the number of frames. 
-        frames = cap.get(cv2.CAP_PROP_FRAME_COUNT) 
-        # number of frames per second.
-        fps = int(cap.get(cv2.CAP_PROP_FPS)) 
-        seekFrame=0
-        while seekFrame < (frames-fps):
-                ret,test_img=cap.read()# captures frame and returns boolean value and captured image
-                if ret:
-                    gray_img= cv2.cvtColor(test_img, cv2.COLOR_BGR2GRAY)
-                    framesList.append(gray_img)
-                jump =seekFrame+fps*secondJump
-                cap.set(cv2.CAP_PROP_POS_FRAMES,jump)
-                seekFrame=jump
-        return framesList
+
+
+class ExtractFrames:
+        @staticmethod
+        def extract_frames(video,secondJump):
+            framesList = []
+            cap=cv2.VideoCapture(video)
+            if not cap.isOpened():
+                print('cap was not opened')
+                cap.open(video)
+            # count the number of frames. 
+            frames = cap.get(cv2.CAP_PROP_FRAME_COUNT) 
+            # number of frames per second.
+            fps = int(cap.get(cv2.CAP_PROP_FPS)) 
+            seekFrame=0
+            while seekFrame < (frames-fps):
+                    ret,test_img=cap.read()# captures frame and returns boolean value and captured image
+                    if ret:
+                        gray_img= cv2.cvtColor(test_img, cv2.COLOR_BGR2GRAY)
+                        framesList.append(gray_img)
+                    jump =seekFrame+fps*secondJump
+                    cap.set(cv2.CAP_PROP_POS_FRAMES,jump)
+                    seekFrame=jump
+            return framesList
 
 
 
 import dlib
 import cv2
 class DetectFaces:
-    def __init__(self,frames):
-        self.frames = frames
+    def __init__(self):
         self.detector = dlib.get_frontal_face_detector()
     
     
-    def detect(self):
+    def detect(self,frames):
         faceFrames =[]
-        for frame in self.frames:
+        for frame in frames:
             faces_detected = self.detector(frame, 1)
             print("Number of faces detected: {}".format(len(faces_detected)))
             for face in faces_detected:
@@ -63,19 +67,17 @@ from keras.preprocessing import image
 
 
 class EmotionModel:
-    def __init__(self,facesImages):
-        self.facesImages = facesImages
-                #load model /content/gdrive/MyDrive/Emotion-detection-latest
+    def __init__(self):
+                #load model
         self.model = model_from_json(open("gdrive/MyDrive/Video Bite - Grad/Projects/Emotion-detection-latest/fer.json", "r").read())
         #load weights
         self.model.load_weights('gdrive/MyDrive/Video Bite - Grad/Projects/Emotion-detection-latest/fer.h5')
 
 
-    def predict(self):
+    def predict(self,facesImages):
         arr = [0] * 7
-        for faceImage in self.facesImages:
-            roi_gray=cv2.resize(faceImage,(48,48))
-            #cv2_imshow(roi_gray)
+        for frameImage in facesImages:
+            roi_gray=cv2.resize(frameImage,(48,48))
             img_pixels = image.img_to_array(roi_gray)
             img_pixels = np.expand_dims(img_pixels, axis = 0)
             img_pixels /= 255
@@ -90,4 +92,6 @@ class EmotionModel:
             print(predicted_emotion)
             arr[max_index]= arr[max_index]+1
 
-        return arr
+        return arr      
+
+
